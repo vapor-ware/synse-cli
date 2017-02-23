6f911d58f48ae888e3a9348ac09c8f51d0ec62ed
Tim <timfall+github@gmail.com>
2017-02-22 16:35:34 -0800

    Finished power commands

--- commands/power.go
+++ commands/power.go
@@ -1,7 +1,7 @@
 package commands
 import (
   "os"
-  //"fmt"
+  "fmt"
   "reflect"
   "strconv"
 
@@ -32,9 +32,10 @@ func ListPower(vc *client.VeshClient) ([][]string, error) {
     boardsValuePtr := boardsPtr.Elem()
     for j := 0; j < boardsValuePtr.Len(); j++ {
       tablerow := make([]string, 0)
-      tablerow = append(tablerow, scanResponse.Racks[i].Boards[j].BoardID)
       rack_id := scanResponse.Racks[i].RackID
       board_id := scanResponse.Racks[i].Boards[j].BoardID
+      tablerow = append(tablerow, rack_id)
+      tablerow = append(tablerow, scanResponse.Racks[i].Boards[j].BoardID) // Switch this to the variable
       responseData := &powerResponse{}
       resp, err := vc.Sling.New().Path(powerpath).Path(rack_id + "/").Path(board_id + "/").Get(device_id).ReceiveSuccess(responseData) // Add error reporting
       if resp.StatusCode != 200 { // This is not what I meant by "error reporting"
@@ -54,10 +55,60 @@ func ListPower(vc *client.VeshClient) ([][]string, error) {
 
 func PrintListPower(vc *client.VeshClient) error {
   table := tablewriter.NewWriter(os.Stdout)
-  table.SetHeader([]string{"Board", "Input Power", "Power Ok?"})
+  table.SetHeader([]string{"Rack", "Board", "Input Power", "Power Ok?"})
   table.SetBorder(false)
+  table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+  table.SetCenterSeparator("|")
+  table.SetAutoMergeCells(true)
   powerList, _ := ListPower(vc) // Add error reporting
   table.AppendBulk(powerList)
   table.Render()
   return nil
+}
+
+func GetPower(vc *client.VeshClient, rack_id, board_id string) ([]string, error) {
+  responseData := &powerResponse{}
+  resp, err := vc.Sling.New().Path(powerpath).Path(rack_id + "/").Path(board_id + "/").Get(device_id).ReceiveSuccess(responseData) // Add error reporting
+  if resp.StatusCode != 200 { // This is not what I meant by "error reporting"
+    return nil, err
+  }
+  tablerow := make([]string, 0)
+  tablerow = append(tablerow, rack_id, board_id, strconv.FormatFloat(responseData.InputPower, 'G', -1, 64), strconv.FormatBool(responseData.OverCurrent), strconv.FormatBool(responseData.PowerOK), responseData.PowerStatus)
+  return tablerow, nil
+}
+
+func PrintGetPower(vc *client.VeshClient, rack_id, board_id string) error {
+  table := tablewriter.NewWriter(os.Stdout)
+  table.SetHeader([]string{"Rack", "Board", "Input Power", "Over Current?", "Power Ok?", "PowerStatus"})
+  table.SetBorder(false)
+  table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+  table.SetCenterSeparator("|")
+  table.SetAlignment(tablewriter.ALIGN_CENTER)
+  tablerow, err := GetPower(vc, rack_id, board_id)
+  table.Append(tablerow)
+  table.Render()
+  return err // Add error reporting
+}
+
+func SetPower(vc *client.VeshClient, rack_id, board_id, power_status string) (string, error) {
+  responseData := &powerResponse{}
+  resp, err := vc.Sling.New().Path(powerpath).Path(rack_id + "/").Path(board_id + "/").Path(device_id + "/").Get(power_status).ReceiveSuccess(responseData) // Add error reporting
+  if resp.StatusCode != 200 { // This is not what I meant by "error reporting"
+    return "", err
+  }
+  if err == nil && power_status == "cycle" { // This should check if successful
+    return power_status, err
+  }
+  return responseData.PowerStatus, err
+}
+
+func PrintSetPower(vc *client.VeshClient, rack_id, board_id, power_status string) error {
+  status, err := SetPower(vc, rack_id, board_id, power_status)
+  if err == nil && status == "cycle" {
+    fmt.Printf("Power successfully %sd\n", status)
+  }
+  if err == nil && status != "cycle" {
+    fmt.Printf("Power set to %s\n", status)
+  }
+  return err
 }
