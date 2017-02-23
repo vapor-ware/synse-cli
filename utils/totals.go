3e4eff6f2cac44046eb603b8f126d56a7c3fd359
Tim <timfall+github@gmail.com>
2017-02-23 14:59:16 -0800

    Added progress bars to temperature
    I’m not really happy with the way this works. The progress bar remains
    at the end of the run and has weird artifacts after exiting. It
    displays an incorrect percentage after exiting and will sometimes screw
    up the table render (but not consistently).  It would be nice to add
    network stats as well.
    
    This also contains a fix for correctly referencing the vapor host url
    and sets a default.

---
+++ utils/totals.go
@@ -0,0 +1,24 @@
+package utils
+import (
+  "reflect"
+
+)
+
+func TotalElemsNum() int {
+  scanResponse, _ := UtilScanOnly() // Add error checking
+  var total = 0
+  racksPtr := reflect.ValueOf(&scanResponse.Racks)
+  racksValuePtr := racksPtr.Elem()
+  for i := 0; i < racksValuePtr.Len(); i++ {
+    boardsPtr := reflect.ValueOf(&scanResponse.Racks[i].Boards)
+    boardsValuePtr := boardsPtr.Elem()
+    for j := 0; j < boardsValuePtr.Len(); j++ {
+      devicesPtr := reflect.ValueOf(&scanResponse.Racks[i].Boards[j].Devices)
+      devicesValuePtr := devicesPtr.Elem()
+      for k := 0; k < devicesValuePtr.Len(); k++ {
+        total ++
+      }
+    }
+  }
+  return total
+}
