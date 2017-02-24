515110bd3a6f536160f7e5cd19bad09b5e32a12f
Tim <timfall+github@gmail.com>
2017-02-23 16:50:29 -0800

    Boottarget and clean up scan.

--- commands/scan.go
+++ commands/scan.go
@@ -6,8 +6,11 @@ import (
   "reflect"
 
   "github.com/vapor-ware/vesh/client"
+  "github.com/vapor-ware/vesh/utils"
 
   "github.com/olekukonko/tablewriter"
+  "github.com/sethgrid/multibar"
+
 )
 
 const Scanpath = "scan"
@@ -46,21 +49,41 @@ func Scan(vc *client.VeshClient) (*scanResponse, error) {
     return status, err
   }
   fmt.Println("API reported status ok")
-  otherthingy := status.Racks[0].Boards[2]
-  stPtr := reflect.ValueOf(&otherthingy.Devices)
-  stotherPtr := stPtr.Elem()
-  table := tablewriter.NewWriter(os.Stdout)
-  table.SetHeader([]string{"Device ID", "Device Info", "Device Type"})
-  table.SetBorder(false)
-  data := make([][]string, stotherPtr.Len())
-  for i := 0; i < stotherPtr.Len(); i ++ {
-    data[i] = make([]string, 3)
-    stPtr2 := reflect.ValueOf(&otherthingy.Devices[i])
-    stotherPtr2 := stPtr2.Elem()
-    for j := 0; j < stotherPtr2.NumField(); j++ {
-      data[i][j] = stotherPtr2.Field(j).String()
+  totaltouched := 0
+  progressBar, _ := multibar.New()
+  go progressBar.Listen()
+  polling := progressBar.MakeBar(utils.TotalElemsNum(), "Polling infrastructure")
+  data := make([][]string, 10000)
+  racksPtr := reflect.ValueOf(&status.Racks)
+  racksValuePtr := racksPtr.Elem()
+  for i := 0; i < racksValuePtr.Len(); i++ {
+    boardsPtr := reflect.ValueOf(&status.Racks[i].Boards)
+    boardsValuePtr := boardsPtr.Elem()
+    for j := 0; j < boardsValuePtr.Len(); j++ {
+      devicesPtr := reflect.ValueOf(&status.Racks[i].Boards[j].Devices)
+      devicesValuePtr := devicesPtr.Elem()
+      for k := 0; k < devicesValuePtr.Len(); k++ {
+        devicePtr := reflect.ValueOf(&status.Racks[i].Boards[j].Devices[i])
+        deviceValuePtr := devicePtr.Elem()
+        tablerow := make([]string, 0)
+        data = append(data, nil)
+        rack_id := status.Racks[i].RackID
+        board_id := status.Racks[i].Boards[j].BoardID
+        tablerow = append(tablerow, rack_id, board_id)
+        for l := 0; l < deviceValuePtr.NumField(); l++ {
+          tablerow = append(tablerow, deviceValuePtr.Field(l).String())
+          polling(totaltouched)
+        }
+        data[totaltouched] = append(data[totaltouched], tablerow...)
+        totaltouched ++
+      }
     }
   }
+  table := tablewriter.NewWriter(os.Stdout)
+  table.SetHeader([]string{"Rack", "Board", "Device ID", "Device Info", "Device Type"})
+  table.SetBorder(false)
+  table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+  table.SetCenterSeparator("|")
   table.AppendBulk(data)
   table.Render()
   return status, nil
