647564260f9de0f73c317f07e17ce232bd2fb543
Tim <tim@vapor.io>
2017-03-21 13:04:40 -0700

    Documentation for the client package.

--- client/requestclient.go
+++ client/requestclient.go
@@ -1,3 +1,7 @@
+// Client extends the https://github.com/dghubble/sling package to provide a
+// RESTful client to the openDCRE endpoints. The base url path is constructed
+// from the configured openDCRE url as well as the type and version of the API.
+// All new queires within vesh should be using an instance of this client.
 package client
 
 import (
@@ -8,8 +12,12 @@ import (
 	"github.com/dghubble/sling"
 )
 
+// Empty variable to store the content of the VESH_HOST env variable.
 var VeshHostPtr = ""
 
+// constructUrl builds the full url string from the host base, endpoint type
+// (openDCRE), and API version number. Endpoint paths can be extended off of
+// this base.
 func constructUrl() string {
 	var vaporBase = fmt.Sprint(VeshHostPtr)
 	var vaporPort = strconv.Itoa(5000)
@@ -19,10 +27,13 @@ func constructUrl() string {
 	return CompleteBase
 }
 
+// VeshClient extends https://github.com/dghubble/sling
 type VeshClient struct {
 	Sling *sling.Sling
 }
 
+// New constructs a new instance of VeshClient after calling constructUrl to
+// contruct the base.
 func New() *VeshClient {
 	cb := constructUrl()
 	return &VeshClient{
