56ef419e8a951eb55b9844121455bd2ea974cb3b
Tim <tim@vapor.io>
2017-04-04 12:36:25 -0700

    Multiple authors.

--- vesh.go
+++ vesh.go
@@ -22,7 +22,8 @@ func main() {
 	app.Name = "vesh"
 	app.Usage = "Vapor Edge Shell"
 	app.Version = "0.0.1"
-	app.Author = "Tim Fall <tim@vapor.io>"
+	app.Authors = []cli.Author{{Name: "Tim Fall", Email: "tim@vapor.io"},
+		{Name: "Thomas Rampelberg", Email: "thomasr@vapor.io"}}
 
 	app.Commands = commands.Commands
 	//app.CommandNotFound = commands.CommandNotFound
