0061c52ceda01d4e6feea588fd2aa48a120aea1d
Erick Daniszewski <edaniszewski@gmail.com>
2018-02-07 08:42:46 -0500

    add basic filtering to scan command

--- flags/command.go
+++ flags/command.go
@@ -10,3 +10,9 @@ var OutputFlag = cli.StringFlag{
 	Value: "yaml",
 	Usage: "set the output format of the command",
 }
+
+// FilterFlag is the flag for setting a filter on a command's output.
+var FilterFlag = cli.StringFlag{
+	Name:  "filter, f",
+	Usage: "set a filter for the output results",
+}
