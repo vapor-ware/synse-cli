c9cda62abf25430b653561d1c4e25093ea4e69de
Matthew Hink <matthew_hink@yahoo.com>
2018-03-02 12:05:16 -0800

    test

--- commands/fan_test.go
+++ commands/fan_test.go
@@ -13,19 +13,23 @@ func init() {
 }
 
 // Could be that this one is failing, causing subsequent failures (?)
-/*
 func TestListFan(t *testing.T) {
+  t.Logf("TestListFan\n")
+  /*
 	err := PrintListFan()
 	if err != nil {
 		t.Error(err)
 	}
+  */
 }
 
 func TestGetFan(t *testing.T) {
+  t.Logf("TestGetFan\n")
+  /*
 	args := utils.GetDeviceArgs{RackID: "rack_1", BoardID: "40000001"}
 	err := PrintGetFan(args)
 	if err != nil {
 		t.Error(err)
 	}
+  */
 }
-*/
