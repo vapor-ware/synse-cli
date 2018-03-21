869bd62862d820c476de240e5c89c61150f59927
Erick Daniszewski <edaniszewski@gmail.com>
2018-03-21 13:35:56 -0400

    code review updates

--- pkg/commands/completion.go
+++ pkg/commands/completion.go
@@ -3,13 +3,23 @@ package commands
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/urfave/cli"
 	"github.com/vapor-ware/synse-cli/pkg/utils"
 )
 
-var bashrc = "$HOME/.bashrc"
-var zshrc = "$HOME/.zshrc"
+var (
+	autocompletePath = "/etc/bash_completion.d"
+
+	// bash
+	bashProfile = "$HOME/.bash_profile"
+	bashrc      = "$HOME/.bashrc"
+
+	// zsh
+	zshrc = "$HOME/.zshrc"
+)
 
 // bashCompletion is the bash completion function for Synse. This is taken
 // from: https://github.com/urfave/cli/blob/master/autocomplete/bash_autocomplete
@@ -102,23 +112,51 @@ func cmdCompletion(c *cli.Context) error {
 
 // cmdEnable is the action for enabling a shell completion.
 func cmdEnable(c *cli.Context, shell string) (err error) {
+
+	switch o := runtime.GOOS; o {
+	case "darwin":
+		return darwinCompletion(c, shell)
+	case "linux":
+		return linuxCompletion(c, shell)
+	default:
+		return fmt.Errorf("synse CLI does not yet support %s systems", o)
+	}
+}
+
+// getFileOrDir returns the expanded path for the first path (file or
+// dir) found to exist out of the ones passed in as parameters. If none
+// of the given paths exist, an error is returned.
+func getFileOrDir(paths ...string) (string, error) {
+	for _, p := range paths {
+		exp := os.ExpandEnv(p)
+		_, err := os.Stat(exp)
+		// if the path exists, return its expanded name
+		if err == nil {
+			return exp, nil
+		}
+	}
+	return "", fmt.Errorf("none of the expected files/dirs were found: %v", paths)
+}
+
+// darwinCompletion sets up Synse CLI completion on darwin systems.
+func darwinCompletion(c *cli.Context, shell string) (err error) {
 	var file string
 	switch shell {
 	case "bash":
-		file = os.ExpandEnv(bashrc)
+		file, err = getFileOrDir(bashProfile, bashrc)
+		if err != nil {
+			return err
+		}
 	case "zsh":
-		file = os.ExpandEnv(zshrc)
+		file, err = getFileOrDir(zshrc)
+		if err != nil {
+			return err
+		}
 	default:
 		return fmt.Errorf("unsupported shell specified: %v", shell)
 	}
 
-	// check that the rc file exists
-	_, err = os.Stat(file)
-	if err != nil {
-		return err
-	}
-
-	// the line that will be added to the rc file
+	// the line that will be added to the file
 	complete := `
 # synse completion
 eval "$(synse completion --%s-completion)"
@@ -136,5 +174,46 @@ eval "$(synse completion --%s-completion)"
 		return err
 	}
 
+	_, err = c.App.Writer.Write([]byte(
+		fmt.Sprintf("You can now run `source %s`\n", file)),
+	)
 	return
+}
+
+// linuxCompletion sets up Synse CLI completion on linux systems.
+func linuxCompletion(c *cli.Context, shell string) (err error) {
+	var file string
+	var contents string
+	switch shell {
+	case "bash":
+		file, err = getFileOrDir(autocompletePath)
+		if err != nil {
+			return err
+		}
+		file = filepath.Join(file, "synse") // /etc/bash_completion.d/synse
+		contents = bashCompletion
+	case "zsh":
+		// FIXME (etd) - I don't know how zsh is setup on linux, so for now
+		// will not do this.
+		return fmt.Errorf("zsh completion is not yet supported on linux")
+	default:
+		return fmt.Errorf("unsupported shell specified: %v", shell)
+	}
+
+	// Create the completion file
+	out, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer out.Close() // nolint
+
+	_, err = out.Write([]byte(contents))
+	if err != nil {
+		return err
+	}
+
+	_, err = c.App.Writer.Write([]byte(
+		fmt.Sprintf("You can now run `source %s`\n", file)),
+	)
+	return err
 }
