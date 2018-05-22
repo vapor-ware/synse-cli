cbd027476df8b0e99a7aa4317d937db6b03a46ee
Erick Daniszewski <edaniszewski@gmail.com>
2018-05-22 08:48:56 -0400

    fix some output formatting inconsistencies (#169)

--- pkg/scheme/hosts_list.go
+++ pkg/scheme/hosts_list.go
@@ -2,7 +2,7 @@ package scheme
 
 // ListHostOutput defines the scheme for the data output by a "list host" command.
 type ListHostOutput struct {
-	Active  bool
-	Name    string
-	Address string
+	Active  bool   `json:"active"`
+	Name    string `json:"name"`
+	Address string `json:"address"`
 }
