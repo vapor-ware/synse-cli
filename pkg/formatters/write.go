f3a3cb5b675ec5fcc9f353cc69f2b580e842f0f2
Hoanh An <hoanhan@bennington.edu>
2018-06-26 17:16:22 -0400

    clean up write scheme

--- pkg/formatters/write.go
+++ pkg/formatters/write.go
@@ -7,11 +7,6 @@ import (
 	"github.com/vapor-ware/synse-cli/pkg/scheme"
 )
 
-// const (
-// 	// the pretty output format for write requests
-// 	prettyWrite = "{{.Transaction}}\t{{.Context.Action}}\t{{$n := len .Context.Data}}{{range $i, $e := .Context.Data}}{{.}}{{if lt (plus1 $i) $n}}, {{end}}{{end}}\n"
-// )
-
 // newWriteFormat is the handler for write commands that is used by the
 // Formatter to add new write data to the format context.
 func newWriteFormat(data interface{}) (interface{}, error) {
@@ -31,7 +26,6 @@ func newWriteFormat(data interface{}) (interface{}, error) {
 // for write command output.
 func NewWriteFormatter(c *cli.Context) *Formatter {
 	f := NewFormatter(c, newWriteFormat)
-	// f.Template = prettyWrite
 	f.Decoder = &scheme.WriteTransaction{}
 
 	return f
