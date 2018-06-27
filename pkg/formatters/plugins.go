643b002b745444a18535d79de55d624914e3dcf6
Hoanh An <hoanhan@bennington.edu>
2018-06-27 16:55:58 -0400

    update plugins scheme to get more details about plugins, health and version from server

--- pkg/formatters/plugins.go
+++ pkg/formatters/plugins.go
@@ -7,6 +7,11 @@ import (
 	"github.com/vapor-ware/synse-cli/pkg/scheme"
 )
 
+const (
+	// the pretty output for plugins requests
+	prettyPlugins = "{{.Name}}\t{{.Network.Protocol}}\t{{.Network.Address}}\t{{.Version.Version}}\t{{.Health.Status}}\n"
+)
+
 // newPluginsFormat is the handler for plugins commands that is used by the
 // Formatter to add new plugin data to the format context.
 func newPluginsFormat(data interface{}) (interface{}, error) {
@@ -35,6 +40,7 @@ func newPluginsFormat(data interface{}) (interface{}, error) {
 // for the plugins command.
 func NewPluginsFormatter(c *cli.Context) *Formatter {
 	f := NewFormatter(c, newPluginsFormat)
+	f.Template = prettyPlugins
 	f.Decoder = &scheme.Plugin{}
 
 	return f
