bb87c88adee35a185beb9f36bb3dec27501c5823
Hoanh An <hoanhan@bennington.edu>
2018-06-28 08:58:49 -0400

    update read test cases

--- pkg/scheme/read.go
+++ pkg/scheme/read.go
@@ -2,23 +2,23 @@ package scheme
 
 // Read is the scheme for the Synse Server "read" endpoint response.
 type Read struct {
-	Kind string     `json:"kind"`
-	Data []ReadData `json:"data"`
+	Kind string     `json:"kind" yaml:"kind"`
+	Data []ReadData `json:"data" yaml:"data"`
 }
 
 // ReadData describes reading data.
 type ReadData struct {
-	Value     interface{} `json:"value"`
-	Timestamp string      `json:"timestamp"`
-	Type      string      `json:"type"`
-	Info      string      `json:"info"`
-	Unit      OutputUnit  `json:"unit"`
+	Value     interface{} `json:"value" yaml:"value"`
+	Timestamp string      `json:"timestamp" yaml:"timestamp"`
+	Type      string      `json:"type" yaml:"type"`
+	Info      string      `json:"info yaml:"info"`
+	Unit      OutputUnit  `json:"unit" yaml:"unit"`
 }
 
 // ReadOutput defines the scheme for the data output by a "read" command.
 type ReadOutput struct {
-	Type      string `json:"type"`
-	Value     string `json:"value"`
-	Unit      string `json:"unit"`
-	Timestamp string `json:"timestamp"`
+	Type      string `json:"type" yaml:"type"`
+	Value     string `json:"value" yaml:"value"`
+	Unit      string `json:"unit" yaml:"unit"`
+	Timestamp string `json:"timestamp" yaml:"timestamp"`
 }
