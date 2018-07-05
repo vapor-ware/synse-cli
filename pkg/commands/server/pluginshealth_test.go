e9ad45f87faea462a92fb7d801736525788df61e
Hoanh An <hoanhan@bennington.edu>
2018-07-05 10:56:13 -0400

    add plugins health handlers and tests

---
+++ pkg/commands/server/pluginshealth_test.go
@@ -0,0 +1,138 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/gotestyourself/gotestyourself/assert"
+	"github.com/gotestyourself/gotestyourself/golden"
+
+	"github.com/vapor-ware/synse-cli/internal/test"
+)
+
+const (
+	// the mocked 200 OK JSON response for the 'plugins health' command.
+	pluginsHealthRespOK = `
+[
+  {
+    "tag":"vaporio\/emulator-plugin",
+    "health":{
+      "timestamp":"2018-06-27T18:30:46.237254715Z",
+      "status":"ok",
+      "message":"",
+      "checks":[
+        {
+          "name":"read buffer health",
+          "status":"ok",
+          "message":"",
+          "timestamp":"2018-06-27T18:30:16.531781924Z",
+          "type":"periodic"
+        },
+        {
+          "name":"write buffer health",
+          "status":"ok",
+          "message":"",
+          "timestamp":"2018-06-27T18:30:16.531781924Z",
+          "type":"periodic"
+        }
+      ]
+    }
+  },
+  {
+    "tag":"vaporio\/unix-plugin",
+    "health":{
+      "timestamp":"2018-06-27T18:30:46.237254715Z",
+      "status":"ok",
+      "message":"",
+      "checks":[
+        {
+          "name":"read buffer health",
+          "status":"ok",
+          "message":"",
+          "timestamp":"2018-06-27T18:30:16.531781924Z",
+          "type":"periodic"
+        },
+        {
+          "name":"write buffer health",
+          "status":"ok",
+          "message":"",
+          "timestamp":"2018-06-27T18:30:16.531781924Z",
+          "type":"periodic"
+        }
+      ]
+    }
+  }
+]`
+
+	// The 500 error JSON response for the Synse Server 'plugins' route
+	// is already mocked in plugins_test.go.
+)
+
+// TestPluginsHealthCommandRequestSuccessYaml tests the 'plugins health' command when it gets
+// a 200 response from Synse Server, with YAML output.
+func TestPluginsHealthCommandRequestSuccessYaml(t *testing.T) {
+	test.Setup()
+
+	mux, server := test.Server()
+	defer server.Close()
+	mux.HandleFunc(
+		"/synse/2.0/plugins",
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			fmt.Fprint(w, pluginsHealthRespOK)
+		},
+	)
+
+	test.AddServerHost(server)
+	app := test.NewFakeApp()
+	app.Commands = append(app.Commands, ServerCommand)
+
+	err := app.Run([]string{
+		app.Name,
+		"--format", "yaml",
+		ServerCommand.Name,
+		pluginsCommand.Name,
+		pluginsHealthCommand.Name,
+	})
+
+	t.Logf("Standard Out: \n%s", app.OutBuffer.String())
+	t.Logf("Standard Error: \n%s", app.ErrBuffer.String())
+
+	assert.Assert(t, golden.String(app.OutBuffer.String(), "pluginshealth.success.yaml.golden"))
+	test.ExpectNoError(t, err)
+}
+
+// TestPluginsHealthCommandRequestSuccessJson tests the 'plugins health' command when it gets
+// a 200 response from Synse Server, with JSON output.
+func TestPluginsHealthCommandRequestSuccessJson(t *testing.T) {
+	test.Setup()
+
+	mux, server := test.Server()
+	defer server.Close()
+	mux.HandleFunc(
+		"/synse/2.0/plugins",
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			fmt.Fprint(w, pluginsHealthRespOK)
+		},
+	)
+
+	test.AddServerHost(server)
+	app := test.NewFakeApp()
+	app.Commands = append(app.Commands, ServerCommand)
+
+	err := app.Run([]string{
+		app.Name,
+		"--format", "json",
+		ServerCommand.Name,
+		pluginsCommand.Name,
+		pluginsHealthCommand.Name,
+	})
+
+	t.Logf("Standard Out: \n%s", app.OutBuffer.String())
+	t.Logf("Standard Error: \n%s", app.ErrBuffer.String())
+
+	assert.Assert(t, golden.String(app.OutBuffer.String(), "pluginshealth.success.json.golden"))
+	test.ExpectNoError(t, err)
+}
