18a4f2c24a3d37a37bc453e375b5cc3cad4f828a
Erick Daniszewski <edaniszewski@gmail.com>
2019-06-11 13:23:19 -0400

    bash completion (#215)

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -62,7 +62,7 @@ var rootCmd = &cobra.Command{
 
 		<underscore>https://github.com/vapor-ware/synse</>
 	`),
-
+	BashCompletionFunction: bashCompletionFunc,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		if flagDebug {
 			log.SetLevel(log.DebugLevel)
