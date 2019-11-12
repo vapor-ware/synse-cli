9170f31047ee39c5556acdf084247c8dd4454ef8
Erick Daniszewski <edaniszewski@gmail.com>
2019-11-12 10:27:55 -0500

    add support for streamed readings over websocket (#225)
    * add support for streamed readings over websocket
    
    * add tests, fix bugs, update tests

---
+++ pkg/cmd/server/stream.go
@@ -0,0 +1,214 @@
+// Synse CLI
+// Copyright (c) 2019 Vapor IO
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package server
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"sort"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/gosuri/uilive"
+	"github.com/spf13/cobra"
+	"github.com/vapor-ware/synse-cli/pkg/utils"
+	"github.com/vapor-ware/synse-cli/pkg/utils/exit"
+	"github.com/vapor-ware/synse-client-go/synse/scheme"
+	"golang.org/x/sync/errgroup"
+)
+
+func init() {
+	cmdStream.Flags().StringSliceVarP(&flagDeviceIds, "id", "i", []string{}, "specify device IDs to use as selectors")
+	cmdStream.Flags().StringSliceVarP(&flagTags, "tag", "t", []string{}, "specify tags to use as device selectors")
+}
+
+var cmdStream = &cobra.Command{
+	Use:   "stream",
+	Short: "Stream current reading data from Synse",
+	Long: utils.Doc(`
+		Get a live stream of reading data as it is read into the system.
+
+		If no flags are specified, this will stream the readings for all devices.
+		The '--id' and '--tag' flags can be used to narrow down the devices for which
+		reading data is streamed back.
+
+		Tags are strings with three components: a namespace (optional), an
+		annotation (optional), and a label (required). They follow the format
+		"namespace/annotation:label". Multiple tags can be specified either
+		by calling the '--tag' flag multiple times, or by providing a comma
+		separated list of tags. For example, the two lines below are equivalent:
+
+		   --tag default/foo --tag default/type:bar
+		   --tag default/foo,default/type:bar
+
+		IDs are the device ID strings. Multiple IDs may be specified either by
+		calling the '--id' flag multiple times, or by providing a comma separated
+		list of IDs. For example, the following two lines are equivalent:
+
+		  --id 285d23c1299ab8e5 --id 33aa9822b188d092fe
+		  --id 285d23c1299ab8e5,33aa9822b188d092fe
+
+		You cannot specify devices both by ID and tag. Doing so will result in
+		an error.
+	`),
+	Run: func(cmd *cobra.Command, args []string) {
+		exiter := exit.FromCmd(cmd)
+
+		// Error out if device IDs and tag selectors are both specified.
+		if len(flagDeviceIds) != 0 && len(flagTags) != 0 {
+			exiter.Err("cannot specify device IDs and device tags together")
+		}
+
+		exiter.Err(serverStream(cmd.OutOrStdout()))
+	},
+}
+
+func serverStream(out io.Writer) error {
+	client, err := utils.NewSynseWebsocketClient(flagContext, flagTLSCert)
+	if err != nil {
+		return err
+	}
+
+	defer client.Close()
+	if err := client.Open(); err != nil {
+		return err
+	}
+
+	// Create a channel which will be used to stop the stream.
+	stop := make(chan struct{}, 1)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT)
+
+	go func() {
+		<-c
+		close(stop)
+	}()
+
+	// Create a channel which will be used to collect the readings as they come in.
+	readings := make(chan *scheme.Read)
+	defer close(readings)
+
+	var g errgroup.Group
+	g.Go(func() error {
+		return client.ReadStream(
+			scheme.ReadStreamOptions{
+				Ids:  flagDeviceIds,
+				Tags: flagTags,
+			},
+			readings,
+			stop,
+		)
+	})
+
+	// Create a terminal writer which refreshes output.
+	writer := uilive.New()
+	writer.Out = out
+	writer.Start()
+
+	if err := writer.Flush(); err != nil {
+		return err
+	}
+
+	tw := utils.NewTabWriter(writer)
+	defer tw.Flush()
+	defer writer.Stop()
+
+	header := []string{"ID", "TYPE", "VALUE", "UNIT", "TIMESTAMP"}
+	rows := map[string]string{}
+	var lock = sync.RWMutex{}
+
+	go func() {
+		for {
+			select {
+			case _, open := <-stop:
+				if !open {
+					return
+				}
+			default:
+				// do nothing
+			}
+
+			str := fmt.Sprintf("\n%s\n", strings.Join(header, "\t"))
+			var data []string
+			lock.RLock()
+			for _, r := range rows {
+				data = append(data, r)
+			}
+			lock.RUnlock()
+			sort.Strings(data)
+			str += strings.Join(data, "\n")
+
+			err := writer.Flush()
+			if err != nil {
+				fmt.Printf("\nerror: %s\n", err)
+				close(stop)
+				return
+			}
+
+			err = tw.Flush()
+			if err != nil {
+				fmt.Printf("\nerror: %s\n", err)
+				close(stop)
+				return
+			}
+
+			_, err = fmt.Fprint(tw, str)
+			if err != nil {
+				fmt.Printf("\nerror: %s\n", err)
+				close(stop)
+				return
+			}
+
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+
+readLoop:
+	for reading := range readings {
+		select {
+		case _, open := <-stop:
+			if !open {
+				break readLoop
+			}
+		default:
+			// do nothing and continue on
+		}
+
+		// Special casing for unit symbol.
+		symbol := reading.Unit.Symbol
+		if symbol == "%" {
+			symbol = "%%"
+		}
+
+		row := strings.Join([]string{
+			reading.Device,
+			reading.Type,
+			fmt.Sprintf("%v", reading.Value),
+			symbol,
+			reading.Timestamp,
+		}, "\t")
+		lock.Lock()
+		rows[fmt.Sprintf("%s-%s", reading.Device, reading.Type)] = row
+		lock.Unlock()
+	}
+	return g.Wait()
+}
---
+++ pkg/cmd/server/stream_test.go
@@ -0,0 +1,76 @@
+// Synse CLI
+// Copyright (c) 2019 Vapor IO
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package server
+
+import (
+	"fmt"
+	"syscall"
+	"testing"
+	"time"
+
+	"bou.ke/monkey"
+	"github.com/vapor-ware/synse-cli/internal/test"
+	"github.com/vapor-ware/synse-cli/pkg/utils"
+	"github.com/vapor-ware/synse-client-go/synse"
+)
+
+func TestCmdStream_badClient(t *testing.T) {
+	patch := monkey.Patch(utils.NewSynseWebsocketClient, func(ctx string, cert string) (synse.Client, error) {
+		return nil, fmt.Errorf("test error message")
+	})
+	defer patch.Unpatch()
+	defer resetFlags()
+
+	result := test.Cmd(cmdStream).Run(t)
+	result.AssertNoErr()
+	result.AssertExited()
+	result.AssertGolden("bad-client.golden")
+}
+
+func TestCmdStream_requestError(t *testing.T) {
+	patch := monkey.Patch(utils.NewSynseWebsocketClient, func(ctx string, cert string) (synse.Client, error) {
+		return test.NewFakeHTTPClientV3Err(), nil
+	})
+	defer patch.Unpatch()
+	defer resetFlags()
+
+	result := test.Cmd(cmdStream).Run(t)
+	result.AssertNoErr()
+	result.AssertExited()
+	result.AssertGolden("request-err.golden")
+}
+
+func TestCmdStream(t *testing.T) {
+	patch := monkey.Patch(utils.NewSynseWebsocketClient, func(ctx string, cert string) (synse.Client, error) {
+		return test.NewFakeHTTPClientV3(), nil
+	})
+	defer patch.Unpatch()
+	defer resetFlags()
+
+	// After 1 second, terminate the stream
+	go func() {
+		time.Sleep(1 * time.Second)
+		_ = syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+
+	}()
+
+	// Unfortunately, there are no great ways of testing the output because of how it is
+	// rendered and how the amount of data rendered may be variable, so for right now the
+	// best we can do is just check that the command exited without error.
+	result := test.Cmd(cmdStream).Run(t)
+	result.AssertNoErr()
+}
