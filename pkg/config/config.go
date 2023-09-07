c6e47e19f802e92982438f26d7d6803b756d8e26
jinal <jinalshah1911@gmail.com>
2023-09-07 19:49:29 +0530

    warning in case of same type and address context added

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -148,7 +148,7 @@ func (c *Config) AddContext(ctx *ContextRecord) error {
 			break
 		} else if context.Context.Address == ctx.Context.Address {
 			if context.Type == ctx.Type {
-				log.Warnf("existing context '%s' with type '%s' already available on the address '%s'", ctx.Name, ctx.Type, ctx.Context.Address)
+				log.Warnf("existing context '%s' with type '%s' already available on the address '%s'", context.Name, context.Type, context.Context.Address)
 			}
 		}
 
